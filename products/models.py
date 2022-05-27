e4d0f2d6864c0025e4b9c7b72a80eabc749e0c4d
rovan89 <devireland89@gmail.com>
2022-05-27 09:32:14 +0000

    Added product views and templates

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
 
     class Meta:
@@ -11,8 +12,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-        def get_friendly_name(self):
-            return self.friendly_name
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
