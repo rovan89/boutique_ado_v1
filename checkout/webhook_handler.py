38c1b7dbbdb8aa5fc7137c4a99ebe5eb1dec1f27
rovan89 <devireland89@gmail.com>
2022-06-04 19:25:33 +0000

    Added checkout form data chaching in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,6 +10,26 @@ class StripeWH_Handler:
         """
         Handle a generic/unknowen/unexpected webhook event
         """
+        return HttpResponse(
+            content=f'Unhandeled webhook recived: {event["type"]}',
+            status=200
+        )
+
+    def handel_payment_intent_succeeded(self, event):
+        """
+        Handle the paymet_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        print(intent)
+        return HttpResponse(
+            content=f'Webhook recived: {event["type"]}',
+            status=200
+        )
+    
+    def handel_payment_intent_failed(self, event):
+        """
+        Handle the paymet_intent.payment_failed webhook from Stripe
+        """
 
         return HttpResponse(
             content=f'Webhook recived: {event["type"]}',
