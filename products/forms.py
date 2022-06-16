8443ebf4bdefc03bc682b7530c606143de15e7db
rovan89 <devireland89@gmail.com>
2022-06-16 18:03:14 +0000

    Product Admin - Fixing the image field part 2

--- products/forms.py
+++ products/forms.py
@@ -8,7 +8,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image')
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
